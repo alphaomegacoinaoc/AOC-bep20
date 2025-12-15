3550ee7980ccecdc723acfa26bdf7e9d95df1cc6
Alpha Omega Coin <95327435+alphaomegacoinaoc@users.noreply.github.com>
2025-12-15 16:40:51 +0530

    Update AOC-Bep20-V2-Token-Multi-sig-Contract.sol

--- contracts/AOC-Bep20-V2-Token-Multi-sig-Contract.sol
+++ contracts/AOC-Bep20-V2-Token-Multi-sig-Contract.sol
@@ -84,6 +84,7 @@ contract MultiSigTokenVault is
     event SignerAdded(address indexed signer);
     event SignerRemoved(address indexed signer);
     event ConfigUpdated(uint256 requiredApprovals, uint256 transactionTimeout);
+    event UpdatedApprovals(uint256 requiredApprovals);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -140,14 +141,18 @@ contract MultiSigTokenVault is
         
         isSigner[signerToRemove] = false;
 
-        //Remove the signer from signers array
-        for (uint256 i = 0; i < signers.length; i++){
-            if(signers[i] == signerToRemove){
-                signers[i] = signers[signers.length - 1];
-                signers.pop();
+        //Find and remove more efficiently
+        uint256 signerIndex = 0;
+        for (uint256 i = 0; i < signers.length; i++) {
+            if (signers[i] == signerToRemove) {
+                signerIndex = i;
                 break;
             }
         }
+        
+        //Move last element to the position of the element to delete
+        signers[signerIndex] = signers[signers.length - 1];
+        signers.pop();
         
         emit SignerRemoved(signerToRemove);
     }
@@ -223,18 +228,18 @@ contract MultiSigTokenVault is
     }
 
     // Emergency controls
-    function pause() external onlySigner {
+    function pause() external onlyMultiSig {
         _pause();
     }
 
-    function unpause() external onlySigner {
+    function unpause() external onlyMultiSig {
         _unpause();
     }
 
     // Configuration: update only the transaction timeout (in seconds)
     function setTransactionTimeout(uint256 newTransactionTimeoutSeconds)
         external
-        onlySigner
+        onlyMultiSig
         whenNotPaused
     {
         require(newTransactionTimeoutSeconds > 0, "Timeout must be > 0");
@@ -315,4 +320,10 @@ contract MultiSigTokenVault is
 
     // // Receive ETH (for contract calls that send ETH)
     // receive() external payable {}
+
+    function updateRequireApprovals(uint256 _newRequiredApprovals) external onlyMultiSig whenNotPaused {
+        require(_newRequiredApprovals > 0 && _newRequiredApprovals <= signers.length, "Invalid Approvals");
+        requiredApprovals = _newRequiredApprovals;
+        emit UpdatedApprovals(_newRequiredApprovals);
+    }
 }
